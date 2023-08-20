aa08e879111e3c5d5a342081da5fd78d78c6f41f
Del426 <Artemisbehan07@gmail.com>
2023-08-20 12:38:18 -0600

    removed turtle and added interactive button

--- Open to site.py
+++ Open to site.py
@@ -1,11 +1,29 @@
+from tkinter import *
 print("hello world")
 
-import turtle            
-wn = turtle.Screen()     
-alex = turtle.Turtle()        
-for _ in range(3600):
-    alex.forward(1)
-    alex.right(1)
 
+#window setup
+import turtle            
+wn = turtle.Screen()  
+wn.setup(400,400)   
 wn.bgpic("test.gif")
 wn.mainloop()
+
+
+#interative button
+def click():
+    if lab["text"] == "Hello":
+        lab["text"]="Bye" 
+    else:
+        lab["text"]=="Hello"
+
+root = Tk()
+root.title("Hello")
+
+lab =Label(root, text="Hello")
+lab.pack()
+
+button = Button(root, text="Click me", command=click)
+button.pack()
+
+root.mainloop()
