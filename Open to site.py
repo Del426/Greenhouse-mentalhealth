c72a7afabc315ca3e3a2bc801db8698cbc1982ff
Del426 <Artemisbehan07@gmail.com>
2023-08-20 12:14:47 -0600

    added window and begining of a loading screen

--- Open to site.py
+++ Open to site.py
@@ -1,1 +1,11 @@
 print("hello world")
+
+import turtle            
+wn = turtle.Screen()     
+alex = turtle.Turtle()        
+for _ in range(3600):
+    alex.forward(1)
+    alex.right(1)
+
+wn.bgpic("test.gif")
+wn.mainloop()
